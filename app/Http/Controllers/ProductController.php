5543f7b4299481a8a7cea9b2d20f798d85c0bec0
Abdulla Adnan <adnankorome123@gmail.com>
2020-12-13 15:54:34 +0530

    api routes changed and validation set

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+use Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -15,6 +17,17 @@ class ProductController extends Controller
 
     public function create(Request $request)
     {
+        $validator=validator::make($request->all(),[
+            'product_name'=>'required',
+            'category_name'=>'required',
+            'prdct_img'=>'required',
+            'price'=>'required',
+            'in_stock'=>'required',
+            'veg_non'=>'required',
+        ]);
+        if ($validator->fails()){
+            return response()->json($validator->errors(),400);
+        }
         $product = DB::table('products')->insert([
             'product_name' => $request->input('product_name'),
             'category_name' => $request->input('category_name'),
@@ -39,6 +52,18 @@ class ProductController extends Controller
 
     public function update(Request $request, $product_id)
     {
+        $validator=validator::make($request->all(),[
+            'product_name'=>'required',
+            'category_name'=>'required',
+            'prdct_img'=>'required',
+            'price'=>'required',
+            'in_stock'=>'required',
+            'veg_non'=>'required',
+
+        ]);
+        if ($validator->fails()){
+            return response()->json($validator->errors(),400);
+        }
         if (DB::table('products')->where('product_id', $product_id)->exists()) {
             $products = DB::table('products')->where('product_id', $product_id)->update([
                 'product_name' => $request->input('product_name'),
@@ -56,6 +81,7 @@ class ProductController extends Controller
                 'from_date' => $request->input('from_date'),
                 'till_date' => $request->input('till_date'),
                 'veg_non' => $request->input('veg_non'),
+                'updated_at'=> Carbon::now()
             ]);
             return response()->json([
                 "message" => "product updated successfully",
